2d82a9f717f43e76030f909291694beac8dee1af
Marc <marcalfaro@live.com>
2020-05-24 15:34:28 +0800

    Final example

--- WinForms-AzureAD/Form1.Designer.vb
+++ WinForms-AzureAD/Form1.Designer.vb
@@ -22,56 +22,99 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnSignIn = New System.Windows.Forms.Button()
+        Me.btnSignOut = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnSignIn
         '
-        Me.Button1.Location = New System.Drawing.Point(81, 75)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Login"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnSignIn.Location = New System.Drawing.Point(72, 12)
+        Me.btnSignIn.Name = "btnSignIn"
+        Me.btnSignIn.Size = New System.Drawing.Size(75, 23)
+        Me.btnSignIn.TabIndex = 0
+        Me.btnSignIn.Text = "SignIn"
+        Me.btnSignIn.UseVisualStyleBackColor = True
+        '
+        'btnSignOut
+        '
+        Me.btnSignOut.Location = New System.Drawing.Point(184, 12)
+        Me.btnSignOut.Name = "btnSignOut"
+        Me.btnSignOut.Size = New System.Drawing.Size(75, 23)
+        Me.btnSignOut.TabIndex = 2
+        Me.btnSignOut.Text = "SignOut"
+        Me.btnSignOut.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(7, 73)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(450, 614)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Black
+        Me.TextBox2.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.White
+        Me.TextBox2.Location = New System.Drawing.Point(463, 73)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox2.Size = New System.Drawing.Size(450, 614)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.WordWrap = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(78, 38)
+        Me.Label1.Location = New System.Drawing.Point(7, 57)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(79, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Access Token:"
         '
-        'Button2
+        'Label2
         '
-        Me.Button2.Location = New System.Drawing.Point(193, 75)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Logout"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(463, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(77, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Identity Claims:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(400, 194)
-        Me.Controls.Add(Me.Button2)
+        Me.ClientSize = New System.Drawing.Size(919, 694)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnSignOut)
+        Me.Controls.Add(Me.btnSignIn)
         Me.DoubleBuffered = True
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
+        Me.Text = "Azure Active Directory Authentication"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnSignIn As Button
+    Friend WithEvents btnSignOut As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label1 As Label
-    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
 End Class
---
+++ WinForms-AzureAD/clsAzureAD.vb
@@ -0,0 +1,138 @@
+﻿'###################################################################################
+'# 
+'#  Microsoft Authentication Library (Azure Active Directory Authentication)
+'# 
+'#  Register your app to Azure Portal under your organization
+'#  Reference: https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v2-windows-desktop
+'# 
+'#  Decode the Access Token received using Microsoft's own JSON Web Token parser: https://jwt.ms/
+'# 
+'#  Coded by Marc
+'#  24 May 2020
+'# 
+'###################################################################################
+
+Option Explicit On
+
+Imports System.IdentityModel.Tokens.Jwt
+Imports Microsoft.Identity.Client
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class clsAzureAD
+
+    'Note: Register your app to Azure Portal under your organization
+    'reference: https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-v2-windows-desktop
+
+    Public Property ClientId As String
+    Public Property TenantId As String
+
+    Private Scopes As IEnumerable(Of String) = {"user.read"}
+
+    Private PublicClientApp As IPublicClientApplication
+
+    Public Sub AzureBuildClient()
+        PublicClientApp = PublicClientApplicationBuilder.Create(ClientId) _
+            .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient") _
+            .WithAuthority(AzureCloudInstance.AzurePublic, TenantId) _
+            .Build()
+    End Sub
+
+    Public Async Function HasSignedIn() As Task(Of Boolean)
+        Dim accounts = Await PublicClientApp.GetAccountsAsync()
+        If accounts.Any() Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Async Function SignIn() As Task(Of AuthenticationResult)
+        Dim authResult As AuthenticationResult = Nothing
+
+        Dim accounts = Await PublicClientApp.GetAccountsAsync()
+        Dim firstAccount = accounts.FirstOrDefault()
+
+        Dim needsSignIn As Boolean = False
+        Try
+            authResult = Await PublicClientApp.AcquireTokenSilent(Scopes, firstAccount).ExecuteAsync()
+        Catch ex As MsalUiRequiredException
+            Debug.WriteLine($"MsalUiRequiredException: {ex.Message}")
+            needsSignIn = True
+
+        Catch ex As Exception
+            Debug.Print($"Error Acquiring Token Silently:{System.Environment.NewLine}{ex}")
+        End Try
+
+        If authResult IsNot Nothing Then
+            Debug.Print(authResult.Account.Username)
+        Else
+            If needsSignIn Then
+                Try
+                    authResult = Await PublicClientApp.AcquireTokenInteractive(Scopes).WithAccount(accounts.FirstOrDefault()).WithPrompt(Prompt.SelectAccount).ExecuteAsync()
+                    Debug.Print(authResult.AccessToken)
+                    Debug.Print(authResult.ToString)
+                Catch msalex As MsalException
+                    Debug.Print($"Error Acquiring Token:{System.Environment.NewLine}{msalex}")
+                End Try
+            End If
+        End If
+
+        Return authResult
+    End Function
+
+    Public Async Function SignOut() As Task
+        Dim accounts = Await PublicClientApp.GetAccountsAsync()
+
+        If accounts.Any() Then
+            Try
+                Await PublicClientApp.RemoveAsync(accounts.FirstOrDefault())
+            Catch ex As MsalException
+                Throw New Exception($"Error signing-out user: {ex.Message}")
+            End Try
+        End If
+    End Function
+
+
+    Public Function JWTokenToJsonString(ByVal accessToken As String, Optional ByVal Prettify As Boolean = False) As String
+        Dim rslt As String = String.Empty
+        Try
+            Debug.Print(accessToken)
+
+            If Not String.IsNullOrWhiteSpace(accessToken) Then
+                Dim jwtTokenHandler = New JwtSecurityTokenHandler
+                Dim jwToken As JwtSecurityToken = jwtTokenHandler.ReadJwtToken(accessToken)
+
+                'Dim _aud As String = jwToken.Audiences.FirstOrDefault
+                'Console.WriteLine("email => " & jwToken.Claims.FirstOrDefault(Function(c) c.Type = "email").Value)
+
+                Debug.Print(jwToken.ToString)
+
+                If Not Prettify Then
+                    rslt = jwToken.ToString
+                Else
+                    Dim jsonStrings As String() = Split(jwToken.ToString, "}.{")
+                    jsonStrings(0) = PrettifyJSON(jsonStrings(0) & "}")
+                    jsonStrings(1) = PrettifyJSON("{" & jsonStrings(1))
+
+                    rslt = $"{jsonStrings(0)}{vbNewLine}.{vbNewLine}{jsonStrings(1)}"
+                End If
+
+            End If
+        Catch ex As Exception
+            Debug.Print(ex.Message)
+        End Try
+        Return rslt
+    End Function
+
+    Public Function PrettifyJSON(ByVal jsonString As String) As String
+        Dim rslt As String = jsonString
+        Try
+            rslt = JToken.Parse(jsonString).ToString(Formatting.Indented)
+        Catch ex As Exception
+            Debug.Print(ex.Message)
+        End Try
+        Return rslt
+    End Function
+
+End Class
